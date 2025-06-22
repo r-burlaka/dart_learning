087c348fe89eca07656ae756b3c55b6e87c11aca
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-06-22 12:01:35 +0300

    feat(DART-06): complete task 10

--- 1_task.dart
+++ 1_task.dart
@@ -149,3 +149,14 @@ void taskNine() {
     i -= 1;
   }
 }//
+void tenth () {
+  for (int i = 1; i<20; i++){
+     if (i % 3 == 0) {
+      continue;
+     }
+     if (i > 15) {
+      break;
+     }
+    print (i);
+ }
+}//
