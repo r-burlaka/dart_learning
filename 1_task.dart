a03dfb57f456ae34bfc8aac243ed8d9ed982a812
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-06-22 11:49:12 +0300

    feat(DART-06): complete task 2

--- 1_task.dart
+++ 1_task.dart
@@ -5,3 +5,9 @@ void main() {
   print('периметр : ${2 * (length + width)}');
   print('повних квадратів : ${(length * width) ~/ 1}');
 } //
+void zavdannyaDva() {
+  int number = 75;
+  print('number > 50 : ${number > 50}');
+  print('number < 100 : ${number < 100}');
+  print('number divides by 5 without remainder : ${number % 5 == 0}');
+} //
