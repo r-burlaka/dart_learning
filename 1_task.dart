f1511f389275729ddfa6d66a7398236d9c7946ab
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-06-22 11:48:09 +0300

    feat(DART-06): complete task 1

---
+++ 1_task.dart
@@ -0,0 +1,7 @@
+void main() {
+  double length = 18.4;
+  double width = 30.9;
+  print('площа : ${length * width}');
+  print('периметр : ${2 * (length + width)}');
+  print('повних квадратів : ${(length * width) ~/ 1}');
+} //
